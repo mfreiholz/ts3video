9aa4fe8e3b9ab450c7a63360b9e62e5a51de135b
mfreiholz <mfreiholz@gmail.com>
2014-12-12 11:44:27 +0100

    Test exe starts defualt server without doing anything else.

--- projects/qtestexe/src/main.cpp
+++ projects/qtestexe/src/main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include "qcorserver.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    QCorServer server(0);
+    server.listen(QHostAddress::Any, 5005);
     return a.exec();
 }
