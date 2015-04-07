54188adbf94193a20e2af88a21edf95da5c86a12
mfreiholz <manuel@insanefactory.com>
2015-04-07 23:17:01 +0200

    Updates about dialog with license and version information.

--- projects/qts3videoclient/src/aboutwidget.cpp
+++ projects/qts3videoclient/src/aboutwidget.cpp
@@ -1,10 +1,13 @@
 #include "aboutwidget.h"
 
+#include "ts3video.h"
+
 ///////////////////////////////////////////////////////////////////////
 
 AboutWidget::AboutWidget(QWidget *parent, Qt::WindowFlags f) :
-QWidget(parent, f)
+  QWidget(parent, f)
 {
   setAttribute(Qt::WA_DeleteOnClose);
   ui.setupUi(this);
+  ui.versionLabel->setText(IFVS_SOFTWARE_VERSION_QSTRING);
 }
