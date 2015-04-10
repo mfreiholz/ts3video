5b5d86f0795f713fc206008bf088a07b6f9471f4
mfreiholz <manuel@insanefactory.com>
2015-04-10 17:15:01 +0200

    removes homepage from this repository.

--- projects/ts3videohomepage/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return array(
-    "default_content" => "/index.html"
-  );
--- projects/ts3videohomepage/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once("D:/Software/xampp/htdocs/source-g/humblecms/lib/humblecms.php");
-//require_once(realpath("../../../humblecms" . DIRECTORY_SEPARATOR . "lib" . DIRECTORY_SEPARATOR . "humblecms.php"));
