3c1f3fde93f54af95ab96a798672d53eb077b257
mfreiholz <manuel@insanefactory.com>
2015-05-10 14:22:45 +0200

    Updates build scripts for plugin.

--- projects/qts3videoclientplugin/cmake/generate-package-ini.cmake
+++ projects/qts3videoclientplugin/cmake/generate-package-ini.cmake
@@ -1,4 +1,13 @@
+#
+# Script to generate ts3_plugin's package.ini
+# The script requires some parameters:
+# - VERSION_MAJOR
+# - VERSION_MINOR
+# - CMAKE_INSTALL_PREFIX
+#
+
+message("Generate package.ini (major=${VERSION_MAJOR}; minor=${VERSION_MINOR})")
 file(WRITE
   ${CMAKE_INSTALL_PREFIX}/ts3_plugin/package.ini
-  "Name = TS3VIDEO\nType = Plugin\nAuthor = Manuel Freiholz\nVersion = 0.1\nPlatforms = win64\n"
+  "Name = TS3VIDEO\nType = Plugin\nAuthor = Manuel Freiholz\nVersion = ${VERSION_MAJOR}.${VERSION_MINOR}\nPlatforms = win32,win64\n"
 )
