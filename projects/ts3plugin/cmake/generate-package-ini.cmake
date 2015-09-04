7f39f16faccf1c3ae24eec9ba2fd0af082c47090
mfreiholz <mf-github@insanefactory.com>
2015-09-04 20:45:04 +0200

    Adds package description.

--- projects/ts3plugin/cmake/generate-package-ini.cmake
+++ projects/ts3plugin/cmake/generate-package-ini.cmake
@@ -9,5 +9,5 @@
 message("Generate package.ini (major=${VERSION_MAJOR}; minor=${VERSION_MINOR})")
 file(WRITE
   ${CMAKE_INSTALL_PREFIX}/ts3_plugin/package.ini
-  "Name = TS3VIDEO\nType = Plugin\nAuthor = Manuel Freiholz\nVersion = ${VERSION_MAJOR}.${VERSION_MINOR}\nPlatforms = win32,win64\n"
+  "Name = TS3VIDEO\nType = Plugin\nAuthor = Manuel Freiholz\nVersion = ${VERSION_MAJOR}.${VERSION_MINOR}\nPlatforms = win32,win64\nDescription = ATTENTION for Updaters:\\nDeactivate the previous installed version of TS3VIDEO in the Settings->Plugins dialog before you continue with the installation.\n"
 )
