d01f3c02d4c0e2f354116143eb79ed99eb40b90e
mfreiholz <mf-github@insanefactory.com>
2015-09-10 18:34:59 +0200

    Updates environment to use Qt 5.4.1

--- env-win-x86-32.bat
+++ env-win-x86-32.bat
@@ -4,5 +4,5 @@ call "%VS140COMNTOOLS%\..\..\Vc\vcvarsall.bat" x86
 rem Prepare basic environment.
 set MF_BUILD_DIR_NAME=build-win-x86-32
 set MF_BUILD_DIR_PATH=%~dp0%MF_BUILD_DIR_NAME%
-set QTDIR=D:\Development\Libraries\Qt\Online\5.3\msvc2013_opengl
+set QTDIR=D:\Development\Libraries\Qt\Qt-5.4.1-x86-32-OpenGL\5.4\msvc2013_opengl
 set PATH=%PATH%;%QTDIR%\bin
