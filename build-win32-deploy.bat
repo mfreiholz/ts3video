0697d27cce68c3f55bd12d42087fc44f386f1f23
mfreiholz <mfreiholz@gmail.com>
2017-11-14 20:36:06 +0100

    Updates build script.
    Updates style guideline.

--- build-win32-deploy.bat
+++ build-win32-deploy.bat
@@ -7,6 +7,8 @@ if not exist %OCS_RELEASE_DIR_PATH%\server mkdir %OCS_RELEASE_DIR_PATH%\server
 copy %OCS_BUILD_DIR_PATH%\projects\videoserver\Release\videoserver.exe %OCS_RELEASE_DIR_PATH%\server
 copy %OCS_SOURCE_DIR%\projects\videoserver\res\default.ini %OCS_RELEASE_DIR_PATH%\server\default.ini.orig
 copy %OCS_SOURCE_DIR%\projects\videoserver\res\logging.conf %OCS_RELEASE_DIR_PATH%\server\logging.conf.orig
+copy %OCS_SOURCE_DIR%\..\ocs-vendor-runtimes\vcredist_x86_2013.exe %OCS_RELEASE_DIR_PATH%\server
+copy %OCS_SOURCE_DIR%\..\ocs-vendor-runtimes\vcredist_x86_2015u3.exe %OCS_RELEASE_DIR_PATH%\server
 windeployqt.exe %OCS_RELEASE_DIR_PATH%\server
 
 echo 
@@ -15,6 +17,8 @@ if not exist %OCS_RELEASE_DIR_PATH%\client mkdir %OCS_RELEASE_DIR_PATH%\client
 copy %OCS_BUILD_DIR_PATH%\projects\videoclient\Release\videoclient.exe %OCS_RELEASE_DIR_PATH%\client
 copy %OCS_SOURCE_DIR%\projects\videoclient\res\default.ini %OCS_RELEASE_DIR_PATH%\client\default.ini.orig
 copy %OCS_SOURCE_DIR%\projects\videoclient\res\logging.conf %OCS_RELEASE_DIR_PATH%\client\logging.conf.orig
+copy %OCS_SOURCE_DIR%\..\ocs-vendor-runtimes\vcredist_x86_2013.exe %OCS_RELEASE_DIR_PATH%\client
+copy %OCS_SOURCE_DIR%\..\ocs-vendor-runtimes\vcredist_x86_2015u3.exe %OCS_RELEASE_DIR_PATH%\client
 windeployqt.exe %OCS_RELEASE_DIR_PATH%\client
 
 echo 
